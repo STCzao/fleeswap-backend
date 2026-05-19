9f1efe051d534bea817db0a218c907a212594c93
Santiago Coronel <santicoronel0183@gmail.com>
2026-05-19 12:17:10 -0300

    Para que chatee la chavalada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
+const http = require("http");
 const app = require("./app");
 const connectDB = require("./config/db");
+const { initSocket } = require("./sockets");
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   await connectDB();
-  app.listen(PORT, () => {
+  const httpServer = http.createServer(app);
+  initSocket(httpServer);
+
+  httpServer.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
   });
 };
